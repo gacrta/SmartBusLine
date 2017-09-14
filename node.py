55768d6a6282acc503398ab69a17c63f402090c6
Gabriel Almeida <gabrielcrta@gmail.com>
2017-09-13 21:27:16 -0300

    [SmartBusLine] Add Route class
    [desc] Add Route class with basic methods
    
    Signed-off-by: Gabriel Almeida <gabrielcrta@gmail.com>

--- node.py
+++ node.py
@@ -4,14 +4,34 @@
 class Node:
 
     def __init__(self, idx=0, label="",
-                 neighbors=None, distance=None):
+                 neighbors=[], distance=[]):
         self.idx = idx
         self.label = label
-        self.neighbors = neighbors
-        self.distance = distance
+        self.neighbors = {}
+        size = len(distance)
+        for i in range(size):
+            self.neighbors.update({neighbors[i]: distance[i]})
 
     def __str__(self):
         return "Node " + self.label
 
     def __repr__(self):
-        return "<Node " + self.idx + " label: " + self.label + ">"
+        return "<Node " + str(self.idx) + " label: " + self.label + ">"
+
+    def getIdx(self):
+        return self.idx
+
+    def getLabel(self):
+        return self.label
+
+    def getNeighbors(self):
+        return self.neighbors.keys()
+
+    def getDistanceOfNode(self, aNode):
+        if aNode.getLabel() in self.getNeighbors():
+            dist = self.neighbors[aNode.getLabel()]
+        elif aNode.getLabel() == self.getLabel():
+            dist = 0
+        else:
+            dist = -1
+        return dist
