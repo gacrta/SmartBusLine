17b1b0192610edd74b567d8b6b06fead80d99f43
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2017-09-29 10:29:43 -0300

    Update individuals.py

--- individuals.py
+++ individuals.py
@@ -8,17 +8,17 @@ import random
 # termine em algum dos terminais
 
 class Individuals:
-	
-	def __init__ (self, label="", routes=[]):
-		self.label = label
-		self.routes = routes
-		
-	def createIndividual ():
-		individuo = [] # array de rotas
-		
-		
-			
-		return individuo
+    
+    def __init__ (self, label="", routes=[]):
+        self.label = label
+        self.routes = routes
+        
+    def createIndividual ():
+        individuo = [] # array de rotas
+        
+        
+            
+        return individuo
 
 # escolhe dois individuos diferentes, escolhe
 # de cada um, aleatoriamente, uma das suas rotas,
@@ -28,33 +28,34 @@ class Individuals:
 # pode receber uma matriz de individuos como parametro
 # ou receber direto dois individ para acasalarem
 # ou dar um "get"/gerar dentro da propria funcao
-	def reproduction ():
-	
-	# usa random para escolher individ (2x se usar matriz)
-	# usa random para dar um "getRoutes" de um individ (pra ser rand a rota pega)
-	# usa random para escolher até qual nó a rota vai ser preservada e appendada a outra
-	
-		return individualSon
-	
-	def mutation():
-		
-		
-		
-		return individualMutated
+    def reproduction ():
+        individualSon = []
+    
+    # usa random para escolher individ (2x se usar matriz)
+    # usa random para dar um "getRoutes" de um individ (pra ser rand a rota pega)
+    # usa random para escolher até qual nó a rota vai ser preservada e appendada a outra
+    
+        return individualSon
+    
+    def mutation():
+        individualMutated = []
+        
+        
+        return individualMutated
 
-	
+    
 
 # criando individuos usando a "startNewRandomRoute"
 
-def individuos (nodesList):
-  individuos = []
-	#termArray = getTermNodesFrom(nodesList) 							# array soh com os term
-	#nodeArray = getNodesFrom(nodesList) 									# array sem os term
-	#termInitial = random.choice(termArray) 	# pega um terminal pra ser o inicio da rota
-	#termEnding = random.choice(termArray) 	# pega um terminal pra ser o fim da rota (se for = init, rota circular)
-	#numRandomNode = random.randint(1,length(nodeArray)) # num aleatorio pra definir tamanho da rota que vai p newRandomRoute
-	
-	return individuos
+    def individuos (nodesList):
+        individuos = []
+    #termArray = getTermNodesFrom(nodesList)                             # array soh com os term
+    #nodeArray = getNodesFrom(nodesList)                                     # array sem os term
+    #termInitial = random.choice(termArray)     # pega um terminal pra ser o inicio da rota
+    #termEnding = random.choice(termArray)     # pega um terminal pra ser o fim da rota (se for = init, rota circular)
+    #numRandomNode = random.randint(1,length(nodeArray)) # num aleatorio pra definir tamanho da rota que vai p newRandomRoute
+    
+        return individuos
 
 # qtos individuos serao criados por turno?
 # e qtos filhos?
@@ -62,27 +63,27 @@ def individuos (nodesList):
 # se dois a dois, repr recebe como parametro ind1, ind2, certo? ou faz um rand dentro de uma matriz
 # de individuos e vai pegando e transando-os?
 
-def reproduction ():
-	
-	#ind1 = getind() 
-	#ind2 = getind()
-	#cromossomo1 = random.choice(ind1) # pega alguma rota do individuo 1
-	#cromossomo2 = random.choice(ind2) # pega alguma rota do individuo 2
-	# dada uma rota de N nos, pensei em usar um x=randint pra pegar um trecho de rota com x nos, 
-	# e outra com N-x nos, e depois ligar elas -> sendo obrigadas a fazerem sentido ou nao? (i.e.,
-	# o ultimo no de uma tem q ser vizinho do primeiro da outra?) 
-	# se nao fizer sentido, isso pode ser cortado na hora dos pesos/selecao (~fitness)
-	
-	# como a rota (cromossomo) eh um array, da pra criar o gene pegando esse array ateh o elemento x
-	#gene = []
-		#for i in random.randint(1,lentgh(cromossomo1)) 
-		#gene.append(cromossomo1[i])
-	#N = lentgh(cromossomo2)
-	#n = random.randint(1,N)
-	#for j in n
-		# gene.append(cromossomo2[N-n+j])
-	
-	
-	# newIndividual = gene1.extend(gene2)
-	
-	return newIndividual
+        def reproduction ():
+    
+    #ind1 = getind() 
+    #ind2 = getind()
+    #cromossomo1 = random.choice(ind1) # pega alguma rota do individuo 1
+    #cromossomo2 = random.choice(ind2) # pega alguma rota do individuo 2
+    # dada uma rota de N nos, pensei em usar um x=randint pra pegar um trecho de rota com x nos, 
+    # e outra com N-x nos, e depois ligar elas -> sendo obrigadas a fazerem sentido ou nao? (i.e.,
+    # o ultimo no de uma tem q ser vizinho do primeiro da outra?) 
+    # se nao fizer sentido, isso pode ser cortado na hora dos pesos/selecao (~fitness)
+    
+    # como a rota (cromossomo) eh um array, da pra criar o gene pegando esse array ateh o elemento x
+    #gene = []
+        #for i in random.randint(1,lentgh(cromossomo1)) 
+        #gene.append(cromossomo1[i])
+    #N = lentgh(cromossomo2)
+    #n = random.randint(1,N)
+    #for j in n
+        # gene.append(cromossomo2[N-n+j])
+    
+    
+    # newIndividual = gene1.extend(gene2)
+    
+            return newIndividual
