902e99778a8279720c783a70fa464175cc4ed285
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2017-09-29 14:02:17 -0300

    genIndividual, reprod & mutation sketched out
    - tudo feito em procedural (pensar em como passar para OO)
    - retomar a createIndividual (ver se isso ta certo)
    - ver se a reproduction ta boa do jeito q ta
    - retomar a mutation (pensar na logica e no q falta)

--- individuals.py
+++ individuals.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import node
+import route
 import random
 
 # gera Individuos -> x rotas dentro da USP
@@ -12,40 +13,77 @@ class Individuals:
     def __init__ (self, label="", routes=[]):
         self.label = label
         self.routes = routes
-        
-    def createIndividual ():
+    
+		def __str__ (self):
+			print ("varias rota")
+		
+    def createIndividual (termNodes, simpleNodes):
         individuo = [] # array de rotas
-        
-        
-            
+				
+				#numOfNodes = 0
+        #allNodes = FALSE
+				#while (!allNodes):
+				# se tiver que passar por todos os nos da rede para sair do loop gerador de individuo
+				# sera algo assim, com "switch(allNodes)" e um contador de nos/
+				# ou armazena cada no novo num array a parte e depois compara
+				
+				# assim eh muito mais facil
+				for i in range (3):
+					(tIni, tEnd) = ( random.choice(termNodes), random.choice(termNodes) )
+					# como saber qdo o individuo nasceu (i.e. parar de gerar rotas pro individ): 
+					# qdo passar por todos nos? qdo atingir um random number entre 1 e x?
+
+					# tem q chamar a gera rotas com um term inicio e fim - (termIni, termFim, array de nos)
+					# (se for o mesmo, seria uma rota circular)
+					routeGene = route.Route.genRoute( tIni, tEnd, simpleNodes )
+					individuo.append(routeGene)
         return individuo
 
-# escolhe dois individuos diferentes, escolhe
-# de cada um, aleatoriamente, uma das suas rotas,
-# escolhe (aleat.) um ponto de cada rota para quebrar
-# e juntar elas
-
+# escolhe dois individuos diferentes e escolhe
+# de cada um, aleatoriamente, umas das suas rotas
+ 
 # pode receber uma matriz de individuos como parametro
 # ou receber direto dois individ para acasalarem
 # ou dar um "get"/gerar dentro da propria funcao
-    def reproduction ():
+
+# passa uma lista de individuos p/ poder escolher RANDOM qual ind dara qual rota
+    def reproduction (indList):
         individualSon = []
+				for i in range(3):
+					ind = random.choice(indList)
+					gene = random.choice(ind)
+					individualSon.append(gene)
     
     # usa random para escolher individ (2x se usar matriz)
     # usa random para dar um "getRoutes" de um individ (pra ser rand a rota pega)
     # usa random para escolher até qual nó a rota vai ser preservada e appendada a outra
     
+		# da maneira que esta, ha o risco de formar um filho igual um pai
+		# pode se atacar isso comparando individuos ou soh deixando ele sofrer pressao seletiva igual
         return individualSon
     
-    def mutation():
+		# recebe o individuo a ser mutado
+		# estou tomando ind como um array; de array (rotas); de array (nos)
+    def mutation(ind, ):
         individualMutated = []
         
+				# como fazer a mutacao? alterando uma rota dentro do indiv(1)? tirar uma das rotas e colocar outra nova(2)?
+				# (2) eh mais facil
+				
+				for i in ind:
+					lucky = random.choice(1,2)
+					if (lucky == 1):
+						individualMutated.append(i)
+					else:
+						routeGene = route.Route.genRoute( tIni, tEnd, simpleNodes )
+						individuo.append(routeGene)
         
         return individualMutated
 
     
+"""
 
-# criando individuos usando a "startNewRandomRoute"
+# criando individuos usando a "startNewRandomRoute" (logica antiga)
 
     def individuos (nodesList):
         individuos = []
@@ -87,3 +125,4 @@ class Individuals:
     # newIndividual = gene1.extend(gene2)
     
             return newIndividual
+"""
