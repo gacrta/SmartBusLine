4654eafbc6162b0fe7a99f0f5475b853ec4546af
Gabriel Almeida <gabrielcrta@gmail.com>
2017-10-12 21:22:08 -0300

    [SmartBusLine] Add terminal and non-terminal route end
    [desc] implemented feature of finding a terminal ending for the route, avoiding repeateble nodes
    
    Signed-off-by: Gabriel Almeida <gabrielcrta@gmail.com>

--- hello.py
+++ hello.py
@@ -4,50 +4,60 @@ Created on Tue Sep 12 19:24:38 2017
 
 @author: gabri
 """
-import route
+from route import Route
 import random
 
 
+MAX_NUMBER_OF_NODES = 10
+ONLY_TERMINAL_ENDING = True
+
 def startNewRandomRoute(nodesList, numberOfNodes):
-    newRoute = route.Route("Random Route")
+    newRoute = Route("Random Route")
     newRoute.addNode(random.choice(nodesList))
     for i in range(numberOfNodes-1):
         addRandomNeighborNode(newRoute, nodesList)
     return newRoute
 
-
-def addRandomNeighborNode(aRoute, nodeList):
+# adds a random neighbor to a given route. returns true if
+# succeeds and false otherwise
+def addRandomNeighborNode(aRoute):
     neighborList = aRoute.getValidNeighbors()
     if len(neighborList) != 0:
         # picks a random neighbor label from last node
         key = random.choice(neighborList)
-        # gets the node from nodeList
-        node = getNodeByLabel(nodeList, key)
-        if node is not None:
-            if aRoute.getNodeByLabel(key) is None:
-                aRoute.addNode(node)
-                print ("Node " + key + " added to route " + aRoute.label)
-                return key
-            else:
-                print ("Node " + key + " already present " + "at route " + aRoute.label)
+        # finds node from database
+        node = Route.findNodeByLabel(key)
+        aRoute.addNode(node)
+        print ("Node " + node.getLabel() + " added to route " + aRoute.getLabel())
+        return True
+    else:
+        # len(neighborList) == 0, no more valid neighbors
+        print ("No valid neighbors.")
+        return False
+
+def startRandomRouteFromTerminal(newRoute):
+    numberOfNodes = newRoute.getNumberOfNodes()
+    if (numberOfNodes == 0):
+        # Inits route with random terminal
+        randomTerminal = random.choice(Route.Terminals)
+        newRoute.addNode(randomTerminal)
+        print ("Terminal " + randomTerminal.getLabel() + " added to route " + newRoute.getLabel())
+    elif (numberOfNodes == MAX_NUMBER_OF_NODES):
+        print ("Route " + newRoute.getLabel() + " ended max nodes")
+        return False
+    wasNodeAdded = addRandomNeighborNode(newRoute)
+    if wasNodeAdded:
+        if newRoute.isTerminalEnded():
+            print ("Route " + newRoute.getLabel() + " ended with terminal " + newRoute.getLastNode().getLabel())
+            return True
+    else:
+        if not ONLY_TERMINAL_ENDING:
+            # allows inner node ending
+            return True
         else:
-            print ("Node " + key + " not found.")
-
-
-def startRandomRouteFromTerminal(nodesList, terminalsList, routeLabel=""):
-    newRoute = route.Route(routeLabel)
-    # Inits route with random terminal
-    newRoute.addNode(random.choice(terminalsList))
-    addRandomNeighborNode(newRoute, nodesList)
-    while 1:
-        if not addRandomNeighborNode(newRoute, nodesList):
-            if addRandomNeighborNode(newRoute, terminalsList):
-                return newRoute
-            else:
-                # TODO
-                print ("[startRandomRouteFromTerminal]: node duplicated")
-                return
-
+            print ("Route " + newRoute.getLabel() + " has no valid end. Deleting " + newRoute.getLastNode().getLabel())
+            newRoute.denyLastNode()
+    return startRandomRouteFromTerminal(newRoute)
 
 def getNodeByLabel(nodesList, nodeLabel):
     for aNode in nodesList:
@@ -56,12 +66,16 @@ def getNodeByLabel(nodesList, nodeLabel):
     print ("getNodeByLabel: Node " + nodeLabel + " not found.")
 
 # main program starts here
-nodes = route.Route.Nodes
+nodes = Route.Nodes
 print (nodes)
-terminals = route.Route.Terminals
+terminals = Route.Terminals
 print (terminals)
 print ("start route array")
 routeArray = []
 for i in range(10):
-    routeArray.append(startRandomRouteFromTerminal(nodes, terminals, str(i)))
-    routeArray[-1].printRouteNodes()
+    newRoute = Route(str(i+1))
+    if startRandomRouteFromTerminal(newRoute):
+        routeArray.append(newRoute)
+        print ("Route " + routeArray[-1].getLabel() + " is VALID and was added.")
+        routeArray[-1].printRouteNodes()
+        del(newRoute)
--- route.py
+++ route.py
@@ -9,12 +9,26 @@ class Route:
     [Nodes, Terminals] = utils.parseJsonString(jsonString)
     del(jsonString)
 
-    def __init__(self, label="", nodes=None):
+    # static method that finds a node at data bank
+    def findNodeByLabel(nodeLabel):
+        allNodes = Route.Nodes + Route.Terminals # concatenates the 2 lists
+        for aNode in allNodes:
+            if aNode.getLabel() == nodeLabel:
+                return aNode
+
+    def __init__(self, label="", nodes=None, deniedNodes=None):
         self.label = label
+        # array of route nodes
         if nodes is None:
             self.nodes = []
         else:
             self.nodes = nodes
+        # array of nodes that results on non terminal ending route 
+        if deniedNodes is None:
+            self.invalid = []
+        else:
+            self.invalid = deniedNodes
+        # route length: used to rank routes
         self.length = self.evalRouteDistance()
 
     def __str__(self):
@@ -23,6 +37,7 @@ class Route:
     def __repr__(self):
         return "<Route " + self.label + " >"
 
+    # simply append a node to nodes list
     def addNode(self, newNode):
         if isinstance(newNode, node.Node):
             self.nodes.append(newNode)
@@ -30,17 +45,23 @@ class Route:
             raise TypeError("The object " + str(type(newNode)) +
                             " is not of type " + str(type(node.Node())))
 
+    # returns the route's last node
     def getLastNode(self):
         if len(self.nodes) != 0:
             return self.nodes[-1]
 
+    # finds a node at this route's nodes list
     def getNodeByLabel(self, nodeLabel):
         for aNode in self.nodes:
             if nodeLabel == aNode.getLabel():
                 return aNode
 
+    # returns the lengh of nodes list
     def getNumberOfNodes(self):
         return len(self.nodes)
+    
+    def getLabel(self):
+        return self.label
 
     def evalRouteDistance(self):
         cDistance = 0
@@ -50,9 +71,28 @@ class Route:
                 cDistance += aNode.getDistanceOfNode(lastNode)
         return cDistance
 
+    # remove the last node and returns it
     def removeLastNode(self):
         if len(self.nodes) != 0:
             return self.nodes.pop()
+
+    # adds a node to invalid list
+    def denyInvalidNode(self, invalidNode):
+        self.invalid.append(invalidNode)
+
+    # removes and invalidates last node
+    def denyLastNode(self):
+        invalidNode = self.removeLastNode()
+        invalidNodeLabel = invalidNode.getLabel()
+        self.denyInvalidNode(invalidNodeLabel)
+        print ("Node " + invalidNode.getLabel() + " in invalid for route " + self.getLabel())
+
+    # returns true if route is terminal ended
+    def isTerminalEnded(self):
+        lastNode = self.getLastNode()
+        if lastNode in Route.Terminals:
+            return True
+        return False
 
     def printRouteNodes(self):
         print ("Print route  " + self.label)
@@ -67,7 +107,7 @@ class Route:
         neighborhood = lastNode.getNeighbors()
         for neighbor in neighborhood:
             neighborNode = self.getNodeByLabel(neighbor)
-            # denys existing inner nodes, but adds a terminal neighbor
-            if ((neighborNode is None) or (neighborNode in Route.Terminals)):
+            # denys existing inner nodes and invalid ones, but adds a terminal neighbor
+            if (((neighborNode is None) or (neighborNode in Route.Terminals)) and (neighbor not in self.invalid)):
                 validNodes.append(neighbor)
         return validNodes
