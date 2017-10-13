c692c5400890ba91873c118b4f8e6be996f2d0e8
Gabriel Almeida <gabrielcrta@gmail.com>
2017-10-13 17:06:59 -0300

    [SmartBusLine] Add RouteGenerator class
    Signed-off-by: Gabriel Almeida <gabrielcrta@gmail.com>

--- route.py
+++ route.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import node
+import random
 import utils.utils as utils
 
 
 class Route:
-    jsonString = utils.readNodesJsonFile()
-    [Nodes, Terminals] = utils.parseJsonString(jsonString)
-    del(jsonString)
-
-    # static method that finds a node at data bank
-    def findNodeByLabel(nodeLabel):
-        allNodes = Route.Nodes + Route.Terminals # concatenates the 2 lists
-        for aNode in allNodes:
-            if aNode.getLabel() == nodeLabel:
-                return aNode
+    """ Class that represents the route data and its methods """
 
     def __init__(self, label="", nodes=None, deniedNodes=None):
         self.label = label
@@ -59,7 +51,7 @@ class Route:
     # returns the lengh of nodes list
     def getNumberOfNodes(self):
         return len(self.nodes)
-    
+
     def getLabel(self):
         return self.label
 
@@ -90,7 +82,7 @@ class Route:
     # returns true if route is terminal ended
     def isTerminalEnded(self):
         lastNode = self.getLastNode()
-        if lastNode in Route.Terminals:
+        if lastNode in RouteGenerator.Terminals:
             return True
         return False
 
@@ -102,12 +94,86 @@ class Route:
 
     # returns a list of available nodes of last neighbor
     def getValidNeighbors(self):
-        validNodes = [];
+        validNodes = []
         lastNode = self.getLastNode()
         neighborhood = lastNode.getNeighbors()
         for neighbor in neighborhood:
             neighborNode = self.getNodeByLabel(neighbor)
             # denys existing inner nodes and invalid ones, but adds a terminal neighbor
-            if (((neighborNode is None) or (neighborNode in Route.Terminals)) and (neighbor not in self.invalid)):
+            if (((neighborNode is None) or (neighborNode in RouteGenerator.Terminals)) and (neighbor not in self.invalid)):
                 validNodes.append(neighbor)
         return validNodes
+
+class RouteGenerator:
+    """ Static class used to create Route objects """
+
+    MAX_NUMBER_OF_NODES = 15
+    ONLY_TERMINAL_ENDING = True
+
+    jsonString = utils.readNodesJsonFile()
+    [Nodes, Terminals] = utils.parseJsonString(jsonString)
+    del(jsonString)
+
+    # static method that finds a node at data bank
+    def findNodeByLabel(nodeLabel):
+        allNodes = RouteGenerator.Nodes + RouteGenerator.Terminals # concatenates the 2 lists
+        for aNode in allNodes:
+            if aNode.getLabel() == nodeLabel:
+                return aNode
+
+    # adds a random neighbor to a given route. returns true if
+    # succeeds and false otherwise
+    def addRandomNeighborNode(aRoute):
+        neighborList = aRoute.getValidNeighbors()
+        if len(neighborList) != 0:
+            # picks a random neighbor label from last node
+            key = random.choice(neighborList)
+            # finds node from database
+            node = RouteGenerator.findNodeByLabel(key)
+            aRoute.addNode(node)
+            print ("Node " + node.getLabel() + " added to route " + aRoute.getLabel())
+            return True
+        else:
+            # len(neighborList) == 0, no more valid neighbors
+            print ("No valid neighbors.")
+            return False
+
+    # receives a route and returns true if a valid route is created
+    def startRandomRouteFromTerminal(newRoute):
+        numberOfNodes = newRoute.getNumberOfNodes()
+        if (numberOfNodes == 0):
+            # Inits route with random terminal
+            randomTerminal = random.choice(RouteGenerator.Terminals)
+            newRoute.addNode(randomTerminal)
+            print ("Terminal " + randomTerminal.getLabel() + " added to route " + newRoute.getLabel())
+        elif (numberOfNodes == RouteGenerator.MAX_NUMBER_OF_NODES):
+            print ("Route " + newRoute.getLabel() + " ended max nodes")
+            return False
+        wasNodeAdded = RouteGenerator.addRandomNeighborNode(newRoute)
+        if wasNodeAdded:
+            if newRoute.isTerminalEnded():
+                print ("Route " + newRoute.getLabel() + " ended with terminal " + newRoute.getLastNode().getLabel())
+                return True
+        else:
+            if not RouteGenerator.ONLY_TERMINAL_ENDING:
+                # allows inner node ending
+                return True
+            else:
+                print ("Route " + newRoute.getLabel() + " has no valid end. Deleting " + newRoute.getLastNode().getLabel())
+                newRoute.denyLastNode()
+        return RouteGenerator.startRandomRouteFromTerminal(newRoute)
+
+    # method that returns a valid route
+    def getNewRoute(label=""):
+        if (label == ""):
+            label = "sample route"
+        routeDone = False
+        newRoute = None
+        while not routeDone:
+            if newRoute != None:
+                print("An invalid route was created and abbandoned.")
+                del(newRoute)
+            newRoute = Route(label)
+            routeDone = RouteGenerator.startRandomRouteFromTerminal(newRoute)
+        print ("Route " + label + " is VALID.")
+        return newRoute
