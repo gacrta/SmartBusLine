d46a9ffec8cc9d48fdf2dfa9c696ee59441ae746
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2017-10-13 19:36:29 -0300

    chegamos no mesmo ponto com individuo e route

--- hello.py
+++ hello.py
@@ -4,15 +4,16 @@ Created on Tue Sep 12 19:24:38 2017
 
 @author: gabri
 """
-import route
+
 import individuals
 
-tamDaPop = 10 # pode ser alterado direto aqui
+tamPop = 10 # pode ser alterado direto aqui
 populacao = []
 ind = [] #individuo
 routeArray = []
-for i in range(10):
-    newRoute = route.RouteGenerator.getNewRoute()
-    routeArray.append(newRoute)
-    print ("Route " + routeArray[-1].getLabel() + " is VALID and was added.")
-    routeArray[-1].printRouteNodes()
+
+for i in range(tamPop):
+    ind = individuals.Individuals(i)
+    populacao.append(ind)
+    #print ("Route " + routeArray[-1].getLabel() + " is VALID and was added.")
+    #routeArray[-1].printRouteNodes()
