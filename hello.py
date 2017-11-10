11001cf7a0d9a854778c81f98ac00b846f498d32
Gabriel Almeida <gabrielcrta@gmail.com>
2017-11-10 18:49:18 -0200

    [SmartBusLine] Add numpy array features

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ Created on Tue Sep 12 19:24:38 2017
 import individuals
 import operator
 import random
+import numpy
 import copy 
 
 MUTATION_RATE = 0.5
@@ -30,6 +31,25 @@ def populationSelect(pop, tamPop):
     popUnder = sortedPop[ int( tamPop/2 ) : ] # takes lowest half population...
     popUnder = random.sample( popUnder, int( 0.1*( tamPop/2 ) ) ) #... and pick 0.1 of them
     return [bestInd] + popOver + popUnder # making a new generation from a half of old pop
+
+def getPopulationArray(population):
+    dt = numpy.dtype([("label", numpy.str_, 20), ("fitness", numpy.float64, 1)])
+    #popArray = numpy.array(dtype=dt)
+    tempList = []
+    for ind in population:
+        tempList.append((ind.label, ind.fitness))
+        #popArray.put((ind.label, ind.fitness))
+    popArray = numpy.array(tempList, dtype = dt)
+    return popArray
+
+def getPopulationMean(popArray):
+    return popArray["fitness"].mean()
+
+def getPopulationMax(popArray):
+    return popArray["fitness"].max()
+
+def getPopulationStd(popArray):
+    return popArray["fitness"].std()
 
 tamPop = 20 # pode ser alterado direto aqui
 populacao = []
