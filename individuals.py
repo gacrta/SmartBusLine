aa4587d05e42ba126588459a2d74b88a9d15e3c0
Gabriel Almeida <gabrielcrta@gmail.com>
2017-12-03 09:01:04 -0200

    [SmartBusLine] Move reproduction to IndCreator
    Signed-off-by: Gabriel Almeida <gabrielcrta@gmail.com>

--- individuals.py
+++ individuals.py
@@ -80,65 +80,6 @@ class Individuals:
             routeList.append(clonedRoute)
         return routeList
 
-    # passa uma lista de individuos p/ poder escolher RANDOM qual ind dara qual rota
-    # filhos serao sempre dois a dois? ou pode ter uns partos frutos de orgia?
-    # TODO: PENSAR MELHOR EM COMO FAZER A REPRODUCAO
-	 # 1. (i) se o metodo recebe um par de pais ou (ii) se recebe a lista com todos
-    # 2. (i) se appenda parte uma rota de um pai na de outro ou (ii) se pega uma rota de cada pai
-	 # 3. (i) retorna um unico filho ou (ii) retorna a lista com a proxima geracao direto
-    @staticmethod
-    def reproduction1(ind1, ind2):
-        individualSon = []
-
-        return individualSon
-
-    @staticmethod
-    def reproduction2(popList):
-
-        # https://www.python-course.eu/python3_deep_copy.php
-        # tutorial copy/deepcopy ~ referencias
-        self.mLogger.debug("Start individual reproduction.")
-        newPopList = copy.copy(popList)
-        # this loop shorts indList removing two of its ind by turn, until it
-        # has 0 or 1 (and with 0/1 elem. could not use remove twice) elements
-        while (len(popList) > 1):
-
-            ind1 = random.choice(popList)
-            ind2 = random.choice(popList)
-            while (ind1 == ind2):
-                ind1 = random.choice(popList)
-                ind2 = random.choice(popList)
-            rL1, rL2 = ind1.cloneIndGenes(), ind2.cloneIndGenes()
-            newInd1, newInd2 = [], []
-            indReproductionDone = False
-            i = 0
-            while(not indReproductionDone):
-                r1, r2 = random.choice(rL1), random.choice(rL2)
-                if r1.getString() != r2.getString():
-                    if i % 2 == 0:
-                        newInd1.append( r1 ), newInd2.append( r2 )
-                    else:
-                        newInd1.append( r2 ), newInd2.append( r1 )
-                    rL1.remove(r1), rL2.remove(r2)
-                elif (len(rL1) == 1):
-                    break
-                if (len(rL1) == 0):
-                    indReproductionDone = True
-                i+=1
-            if (indReproductionDone):
-                #newPopList.append( Individuals(ind1.label+ind2.label, None, newInd1) )
-                #newPopList.append( Individuals(ind2.label+ind1.label, None, newInd2) )
-                popList.remove(ind1)
-                popList.remove(ind2)
-                newPopList.append( Individuals("", None, newInd1) )
-                newPopList.append( Individuals("", None, newInd2) )
-            else:
-                del(newInd1)
-                del(newInd2)
-        #if len(popList) == 1: newPopList.append (Individuals.mutation(popList.pop()))
-        self.mLogger.debug("End individual reproduction.")
-        return newPopList
-
 		# TODO: COMO SERÁ A MUTAÇÃO? (i) um individuo com uma nova rota ou (ii) uma das rotas do individuo alterada?
 		# recebe o individuo a ser mutado
     # estou tomando ind como um array; de array (rotas); de array (nos)
@@ -167,44 +108,20 @@ class Individuals:
     return newIndividual
     """
 
-    # sample method to evaluate fitness
-    # returns the simple median of the lenght of individual routes
-    def evalFitness(self):
-        sumLenght = 0
-        for aRoute in self.genes:
-            sumLenght += aRoute.evalRouteDistance()
-        sumLenght -= self.getLackingNodes()*Individuals.LACKING_NODE_PENALTY
-        return sumLenght/Individuals.numRoutes
-
-    def evalFitness2(self, solutions):
-        sumDemand = 0
-        demandTimesTime = 0
-        unnatendedDemand = 0
-        # solutions has type [[demand, time], ...]
-        for aSolution in solutions:
-            sumDemand += aSolution[0]
-            if aSolution[1] != -1:
-                demandTimesTime += aSolution[0]*aSolution[1]
-            else:
-                unnatendedDemand += aSolution[0]
-        result = demandTimesTime/sumDemand
-        result += self.getLackingNodes()*Individuals.LACKING_NODE_PENALTY
-        result += unnatendedDemand
-        self.fitness = result
-
     # method that evaluates fitness as CHAKROBORTY
-    def evalFitness3(self, K1, xm, K2, K3,
+    def evalFitness(self, K1, xm, K2, K3,
                      ODmatrix, transferTime, minimumPath, averageSpeed):
         self.mLogger.debug("Start individual fitness evaluation.")
-        solutions = self.evalIVT(ODmatrix, transferTime, averageSpeed)
+        if not self.updated:
+            solutions = self.evalIVT(ODmatrix, transferTime, averageSpeed)
 
-        F1 = self.evalF1(solutions, K1, xm, minimumPath, averageSpeed)
-        F2 = self.evalF2(solutions, K2)
-        F3 = self.evalF3(solutions, K3)
+            F1 = self.evalF1(solutions, K1, xm, minimumPath, averageSpeed)
+            F2 = self.evalF2(solutions, K2)
+            F3 = self.evalF3(solutions, K3)
 
-        self.fitness = F1+F2+F3
-        self.updated = True
-        self.mLogger.debug("End individual fitness evaluation.")
+            self.fitness = F1+F2+F3
+            self.updated = True
+            self.mLogger.debug("End individual fitness evaluation.")
 
     # evaluetes the time part of fitness
     def evalF1(self, solutions, K1, xm, minimumPath, averageSpeed):
@@ -325,12 +242,12 @@ class Individuals:
 
         solutions = []  # list that contains solutions
 
-        self.mLogger.debug("Getting common routes.")
+        #self.mLogger.debug("Getting common routes.")
         # searches for common routes between [Ro] and [Rd]
         commonRoutes = route.RouteList.getCommonListElements(originRouteList,
                                                              destinationRouteList)
         if len(commonRoutes) != 0:
-            self.mLogger.debug("Evaluating time from common routes.")
+            #self.mLogger.debug("Evaluating time from common routes.")
             for solutionRoute in commonRoutes:
                 time = solutionRoute.evalRouteTime(originNode, destinationNode,
                                                    averageSpeed)
@@ -339,13 +256,13 @@ class Individuals:
                     solutions.append(time)
             if len(solutions) > 0:
                 return [min(solutions), False]
-        self.mLogger.debug("No common routes found.")
+        #self.mLogger.debug("No common routes found.")
 
         # otherwise, search for common nodes between each element of [Ro]
         # and [Rd]
         for originRoute in originRouteList:
             for destinationRoute in destinationRouteList:
-                self.mLogger.debug("Getting common nodes for transfer.")
+                #self.mLogger.debug("Getting common nodes for transfer.")
                 commonNodes = originRoute.getCommonNodes(destinationRoute)
                 for transferNode in commonNodes:
                     time = self.evalTransitTimeWithTransfer(transferNode,
@@ -359,10 +276,10 @@ class Individuals:
 
         # if a transfer node is found, return the smallest time
         if len(solutions) != 0:
-            self.mLogger.debug("Travel attended.")
+            #self.mLogger.debug("Travel attended.")
             return [min(solutions), True]
 
-        self.mLogger.debug("Travel NOT attended.")
+        #self.mLogger.debug("Travel NOT attended.")
         # else, the demand is unattended
         return [-1, False]
 
@@ -433,6 +350,53 @@ class IndividualCreator:
         lenAllPossibleNodes = len(self.mRouteGenerator.getAllNodes())
         return lenAllPossibleNodes - len(aIndividual.getAllNodes())
 
+    # method that executes reproduction of population
+    def reproduction(self, popList):
+
+        # https://www.python-course.eu/python3_deep_copy.php
+        # tutorial copy/deepcopy ~ referencias
+        self.mLogger.debug("Start individual reproduction.")
+        newPopList = copy.copy(popList)
+        # this loop shorts indList removing two of its ind by turn, until it
+        # has 0 or 1 (and with 0/1 elem. could not use remove twice) elements
+        while (len(popList) > 1):
+
+            ind1 = random.choice(popList)
+            ind2 = random.choice(popList)
+            while (ind1 == ind2):
+                ind1 = random.choice(popList)
+                ind2 = random.choice(popList)
+            rL1, rL2 = ind1.cloneIndGenes(), ind2.cloneIndGenes()
+            newInd1, newInd2 = [], []
+            indReproductionDone = False
+            i = 0
+            while(not indReproductionDone):
+                r1, r2 = random.choice(rL1), random.choice(rL2)
+                if r1.getString() != r2.getString():
+                    if i % 2 == 0:
+                        newInd1.append( r1 ), newInd2.append( r2 )
+                    else:
+                        newInd1.append( r2 ), newInd2.append( r1 )
+                    rL1.remove(r1), rL2.remove(r2)
+                elif (len(rL1) == 1):
+                    break
+                if (len(rL1) == 0):
+                    indReproductionDone = True
+                i+=1
+            if (indReproductionDone):
+                #newPopList.append( Individuals(ind1.label+ind2.label, None, newInd1) )
+                #newPopList.append( Individuals(ind2.label+ind1.label, None, newInd2) )
+                popList.remove(ind1)
+                popList.remove(ind2)
+                newPopList.append( Individuals("", None, newInd1) )
+                newPopList.append( Individuals("", None, newInd2) )
+            else:
+                del(newInd1)
+                del(newInd2)
+        #if len(popList) == 1: newPopList.append (Individuals.mutation(popList.pop()))
+        self.mLogger.debug("End individual reproduction.")
+        return newPopList
+
     # method that gets a individual and may return a mutated one
     def mutation(self, ind):
         self.mLogger.debug("Ind mutation starts.")
@@ -445,7 +409,7 @@ class IndividualCreator:
                 newRoute = self.mRouteGenerator.getNewRoute(str(i+1))
                 indMutated.append(newRoute)
         self.mLogger.debug("Ind mutation ends.")
-        return Individuals(ind.getLabel() + "M", None, indMutated)
+        return Individuals("", None, indMutated)
 
     # method that creates current USP bus situation
     def getCurrentIndividual(self):
